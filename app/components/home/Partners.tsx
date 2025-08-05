b4c2e886c25ef7d8aea81c6793381c18be33ab0f
Wix-Code <ogbonna428alex@gmail.com>
2025-08-05 07:15:01 +0100

    push

--- app/components/home/Partners.tsx
+++ app/components/home/Partners.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Partners = () => {
   return (
     <div className='py-2 bg-[#fcfcfc8f]'>
-      <div className='max-w-[1000px] sm:flex-col md:flex m-auto items-center gap-5'>
+      <div className="max-w-[1000px] mx-auto flex flex-col md:flex-row items-center gap-5 px-4">
         <div className='flex-1'>
           <img className='h-[400px] w-full object-cover' src="https://images.ctfassets.net/7qqwgjna58ct/3n1L55PK6Xj2i6hYZWQc4C/5d003d6f666202796ede2745c3d21836/Flexible_working_in_education_blog_square_image.jpg" alt="" />
         </div>
