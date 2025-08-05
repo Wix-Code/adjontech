c85f1c1d8024c9c2738e61749f4bdb9caf8efcae
Wix-Code <ogbonna428alex@gmail.com>
2025-08-05 08:18:22 +0100

    push

--- app/dummyData.tsx
+++ app/dummyData.tsx
@@ -1,15 +1,15 @@
 export const logos = [
   {
     id: 1,
-    pic: "https://www.techsolutionsinc.com/wp-content/uploads/2021/11/partner-dell.jpg"
+    pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Adobe-logo.svg/2560px-Adobe-logo.svg.png"
   },
   {
     id: 2,
-    pic: "https://www.techsolutionsinc.com/wp-content/uploads/2021/11/partner-dell.jpg"
+    pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcBbjdjpZCyFJMyPLKuyEx67_g8X3KzRmESw&s"
   },
   {
     id: 3,
-    pic: "https://www.techsolutionsinc.com/wp-content/uploads/2021/11/partner-dell.jpg"
+    pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7iT6zhtuoGNOFPkUg2JC7ibXFjnHcqxlShA&s"
   },
   {
     id: 4,
@@ -17,11 +17,11 @@ export const logos = [
   },
   {
     id: 5,
-    pic: "https://www.techsolutionsinc.com/wp-content/uploads/2021/11/partner-dell.jpg"
+    pic: "https://brand.autodesk.com/wp-content/uploads/2025/02/logo-02-variations-primary-421x237@2x.png"
   },
   {
     id: 6,
-    pic: "https://www.techsolutionsinc.com/wp-content/uploads/2021/11/partner-dell.jpg"
+    pic: "https://www.techsolutionsinc.com/wp-content/uploads/2021/11/partner-microsoft.jpg"
   },
 ]
 
