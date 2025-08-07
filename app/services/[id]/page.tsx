4fc2fe5dd37ed713baa0f78aeb36f594e648a4ed
Wix-Code <ogbonna428alex@gmail.com>
2025-08-07 12:40:10 +0100

    push

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import { services } from '@/app/dummyData'
+import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+type Service = {
+  id: number
+  name: string
+  img: string
+  note?: string
+}
+
+const Page = () => {
+  const { id } = useParams()
+  const [service, setService] = useState<Service | null>(null)
+
+  useEffect(() => {
+    if (!id) return; // Skip if id is not ready
+
+    const foundService = services.find((s) => s.id === Number(id))
+
+    if (foundService) {
+      setService(foundService)
+    } else {
+      setService(null)
+    }
+  }, [id])
+
+
+  if (!service) return <div>Loading or service not found...</div>
+
   return (
-    <div>page</div>
+    <div className="p-5 space-y-4">
+      <img src={service.img} alt={service.name} className="h-[300px] object-cover" />
+      <h1 className="text-2xl font-bold capitalize">{service.name}</h1>
+      <p className="text-lg">{service?.note || 'No description available.'}</p>
+    </div>
   )
 }
 
-export default page
+export default Page
