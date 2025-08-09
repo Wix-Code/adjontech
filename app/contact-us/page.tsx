85ec41d7fa2c227a3fae04a81d7e3d6b4559c95e
Wix-Code <ogbonna428alex@gmail.com>
2025-08-09 10:48:11 +0100

    push

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,5 +1,6 @@
 import { Linkedin, MapPin } from 'lucide-react';
 import React from 'react';
+import { FaFacebookF, FaInstagram, FaPhone, FaWhatsapp } from 'react-icons/fa';
 
 const ContactPage = () => {
   return (
@@ -20,26 +21,63 @@ const ContactPage = () => {
       <div className="flex flex-col md:flex-row gap-8 max-w-[900px] mx-auto px-5 pb-10">
         {/* Contact Info */}
         <div className="flex-1 text-center flex flex-col items-center space-y-3">
-          <MapPin className="text-[#ff0022]" />
-          <p className="text-[#4e4e4e] text-[16px] font-semibold">Abjontech Solutions Limited</p>
-          <p className="text-[14px] font-normal">
+          <div data-aos="fade-up"
+            data-aos-delay="800" className="w-full">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8133662.920782439!2d-2.1862792968749947!3d5.490236007479131!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104259b03565895d%3A0x942027305c2068cb!2sAbjontech%20Solutions%20Limited!5e0!3m2!1sen!2sng!4v1754730137131!5m2!1sen!2sng"
+              className="w-full h-[400px]"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+
+          <MapPin data-aos="fade-up"
+            data-aos-delay="800" className="text-[#ff0022]" />
+          <p data-aos="fade-up"
+            data-aos-delay="800" className="text-[#4e4e4e] text-[16px] font-semibold">Abjontech Solutions Limited</p>
+          <p data-aos="fade-up"
+            data-aos-delay="800" className="text-[14px] font-normal">
             Plot 49, MCC Road, Ikenegbu Layout, Owerri, Imo State, Nigeria
           </p>
-          <div>
+          <div data-aos="fade-up"
+            data-aos-delay="800" className='flex items-center gap-5'>
             <a
-              href="https://linkedin.com"
+              href="https://+2348035302307"
               target="_blank"
               rel="noopener noreferrer"
-              className="w-[40px] h-[40px] rounded-full bg-blue-500 flex items-center justify-center text-white hover:bg-blue-600 transition"
+              className="w-[40px] h-[40px] rounded-[8px] bg-green-600 flex items-center justify-center text-white hover:bg-green-500 transition"
             >
-              <Linkedin className="w-[18px]" />
+              <FaPhone className='text-[#FFFFFF] text-[22px]' />
+            </a>
+            <a href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[40px] h-[40px] rounded-[8px] bg-blue-500 flex items-center justify-center text-white hover:bg-blue-600 transition">
+              <FaFacebookF className='text-[#FFFFFF] text-[22px]'/>
+            </a>
+            <a
+              href="https://wa.me/2348035302307"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[40px] h-[40px] rounded-[8px] bg-green-500 flex items-center justify-center text-white hover:bg-green-600 transition"
+            >
+              <FaWhatsapp className="text-[#FFFFFF] text-[22px]" />
+            </a>
+            <a href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[40px] h-[40px] rounded-[8px] bg-blue-500 flex items-center justify-center text-white hover:bg-blue-600 transition">
+              <FaInstagram className='text-[#FFFFFF] text-[22px]' />
             </a>
           </div>
         </div>
 
         {/* Contact Form */}
         <div className="flex-1 space-y-5">
-          <div>
+          <div data-aos="fade-up"
+            data-aos-delay="800">
             <label htmlFor="name" className="text-[14px] text-[#4e4e4e] font-medium block mb-1">
               Full Name
             </label>
@@ -51,7 +89,8 @@ const ContactPage = () => {
             />
           </div>
 
-          <div>
+          <div data-aos="fade-up"
+            data-aos-delay="800">
             <label htmlFor="email" className="text-[14px] text-[#4e4e4e] font-medium block mb-1">
               Email
             </label>
@@ -63,7 +102,8 @@ const ContactPage = () => {
             />
           </div>
 
-          <div>
+          <div data-aos="fade-up"
+            data-aos-delay="800">
             <label htmlFor="subject" className="text-[14px] text-[#4e4e4e] font-medium block mb-1">
               Subject
             </label>
@@ -75,7 +115,8 @@ const ContactPage = () => {
             />
           </div>
 
-          <div>
+          <div data-aos="fade-up"
+            data-aos-delay="800">
             <label htmlFor="message" className="text-[14px] text-[#4e4e4e] font-medium block mb-1">
               Message
             </label>
@@ -85,6 +126,13 @@ const ContactPage = () => {
               placeholder="Write your message here"
             />
           </div>
+          <button
+            data-aos="fade-up"
+            data-aos-delay="800"
+            className="inline-block cursor-pointer bg-black text-white px-6 py-3 font-medium hover:bg-[#333] transition"
+          >
+            Send Message
+          </button>
         </div>
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} font-sans antialiased`}>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
